884370b187969412af08b21906fa0fde43f35a62
Anwesh  Panda <anweshpanda2323@gmail.com>
2026-03-01 23:42:24 +0530

    folder structure commit

---
+++ src/hooks/task.ts
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { Task } from "../models/tasks";
+
+export const useTasks = () => {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const addTask = (task: Task) => {
+    setTasks((prev) => [...prev, task]);
+  };
+
+  return { tasks, addTask };
+};
---
+++ src/services/taskService.ts
@@ -0,0 +1,16 @@
+import { Task } from "../models/tasks";
+
+
+export const createTask = (title: string): Task => ({
+  id: Date.now().toString(),
+  title,
+  completed: false,
+});
+
+export const toggleTaskById = (tasks: Task[], id: string): Task[] =>
+  tasks.map((task) =>
+    task.id === id ? { ...task, completed: !task.completed } : task,
+  );
+
+export const deleteTaskById = (tasks: Task[], id: string): Task[] =>
+  tasks.filter((task) => task.id !== id);
